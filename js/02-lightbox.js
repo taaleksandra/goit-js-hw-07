d2742880ced14ec20aab3e9b61c46977283e0c9e
taaleksandra <olatarnas@gmail.com>
2022-11-14 21:08:50 +0100

    2nd task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+// making gallery elements
+galleryItems.forEach((item) => {
+  const imageLink = document.createElement("a");
+  imageLink.classList.add("gallery__item");
+  imageLink.setAttribute("href", item.original);
+
+  const imagePhoto = document.createElement("img");
+  imagePhoto.classList.add("gallery__image");
+  imagePhoto.src = item.preview;
+  imagePhoto.setAttribute("alt", item.description);
+
+  imageLink.append(imagePhoto);
+  gallery.append(imageLink);
+});
+
+var lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
